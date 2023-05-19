fa1854b4a915f0ba981261b4eaab25f50836a05a
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-05-19 09:59:36 -0700

    WIP: get directions api to bypass CORS

---
+++ src/components/LoadScript.js
@@ -0,0 +1,6 @@
+import { LoadScript } from '@react-google-maps/api';
+
+
+<LoadScript googleMapsApiKey={process.env.REACT_APP_GMAP_KEY}>
+  <Map />
+</LoadScript>
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,107 +1,41 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
-import { Autocomplete } from '@react-google-maps/api';
-import "../styles/GoogleMaps.css";
-import Geocoding from './Geocoding';
-import RandomLocationGenerator from './RandomLocation';
-import UserLocation from './UserLocation';
-import ImageUpload from './UploadPhoto';
+import { GoogleMap, DirectionsService, DirectionsRenderer } from '@react-google-maps/api';
 
-const MapContainer = ({ google, apiKey }) => {
-  const [markers, setMarkers] = useState([]);
-  const [mapMounted, setMapMounted] = useState(false);
-  const [lat, setLat] = useState(null);
-  const [lng, setLng] = useState(null);
-  const [activeMarker, setActiveMarker] = useState(null);
-  const [selectedPlace, setSelectedPlace] = useState({ props: {} });
-  const mapRef = useRef(null);
+function Map() {
+  const [response, setResponse] = useState(null);
 
-  useEffect(() => {
-    setMapMounted(true);
-  }, []);
-
-  const onMapClick = (mapProps, map, clickEvent) => {
-    const newMarker = {
-      lat: clickEvent.latLng.lat(),
-      lng: clickEvent.latLng.lng(),
-    };
-    setMarkers([...markers, newMarker]);
-    setLat(newMarker.lat);
-    setLng(newMarker.lng);
-    setSelectedPlace({});
-  };
-
-  const onMarkerClick = (index) => {
-    const marker = markers[index];
-    setActiveMarker(marker);
-    setSelectedPlace({ props: { index } });
-    deleteMarker();
-  };
-
-  const deleteMarker = () => {
-    const updatedMarkers = [...markers];
-    const index = updatedMarkers.findIndex((marker) => marker === activeMarker);
-    if (index !== -1) {
-      updatedMarkers.splice(index, 1);
-      setMarkers(updatedMarkers);
-      setActiveMarker(null);
+  const directionsCallback = (res) => {
+    if (res !== null) {
+      if (res.status === 'OK') {
+        setResponse(res);
+      } else {
+        console.log('response: ', res);
+      }
     }
-  };
-
-  const handleLocationButtonClick = (location, clickEvent) => {
-    const newMarker = {
-      lat: clickEvent.latLng.lat(),
-      lng: clickEvent.latLng.lng(),
-    };
-    setMarkers([...markers, newMarker]);
-    setLat(newMarker.lat);
-    setLng(newMarker.lng);
-  };
-
-  if (!mapMounted) {
-    return "Loading...";
   }
 
   return (
-    <div className="map-wrapper">
-      <div className="map-container">
-        <Map
-          google={google}
-          zoom={3}
-          initialCenter={{ lat: 39.0902, lng: -95.7129 }}
-          containerStyle={{
-            width: "100%",
-            height: "100%",
-            position: "absolute",
-          }}
-          onClick={onMapClick}
-          ref={mapRef}
-        >
-          {markers.map((marker, index) => (
-            <Marker
-              key={index}
-              position={{ lat: marker.lat, lng: marker.lng }}
-              onClick={() => onMarkerClick(index)}
-              index={index}
-            />
-          ))}
-     
-        </Map>
-      </div>
-      <div className='locationButton'>
-        <RandomLocationGenerator onLocationButtonClick={handleLocationButtonClick} />
-      </div>
-      <div className='locationButton'>
-        <UserLocation onLocationButtonClick={handleLocationButtonClick} />
-      </div>
-      <div className='locationButton'>
-        <ImageUpload />
-      </div>
-      <Geocoding lat={lat} lng={lng} />
-    </div>
-  );
-};
+    <GoogleMap
+      id="directions-example"
+      mapContainerStyle={{ height: "400px", width: "800px" }}
+      zoom={2}
+      center={{ lat: 40.748817, lng: -73.985428 }}
+    >
+      <DirectionsService
+        options={{ 
+          origin: 'Chicago',
+          destination: 'New York',
+          travelMode: 'DRIVING' 
+        }}
+        callback={directionsCallback}
+      />
 
-export default GoogleApiWrapper({
-apiKey: process.env.REACT_APP_GMAP_KEY
-})(MapContainer);
+      {response !== null && (
+        <DirectionsRenderer
+          options={{ 
+            directions: response
+          }}
+        />
+      )}
+    </GoogleMap>
+  );
+}
